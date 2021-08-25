5c244f758595a8b18a1d1174e27b049fa60fc593
Rizki Kurniawan <rizkikurniawan1797@gmail.com>
2021-08-25 22:56:04 +0700

    Implemented gesture

--- lib/main.dart
+++ lib/main.dart
@@ -25,37 +25,59 @@ class _MyHomePageState extends State<MyHomePage> {
   int numTaps = 0;
   int numDoubleTaps = 0;
   int numLongPress = 0;
+
+  double posX = 0.0;
+  double posY = 0.0;
+
   final double boxSize = 150.0;
 
   @override
   Widget build(BuildContext context) {
+    if (posX == 0) {
+      center(context);
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Gesture Detector'),
       ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            setState(() {
-              numTaps++;
-            });
-          },
-          onDoubleTap: () {
-            setState(() {
-              numDoubleTaps++;
-            });
-          },
-          onLongPress: () {
-            setState(() {
-              numLongPress++;
-            });
-          },
-          child: Container(
-            width: boxSize,
-            height: boxSize,
-            decoration: BoxDecoration(color: Colors.red),
+      body: Stack(
+        children: [
+          Positioned(
+            top: posY,
+            left: posX,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  numTaps++;
+                });
+              },
+              onDoubleTap: () {
+                setState(() {
+                  numDoubleTaps++;
+                });
+              },
+              onLongPress: () {
+                setState(() {
+                  numLongPress++;
+                });
+              },
+              onPanUpdate: (DragUpdateDetails details) {
+                setState(() {
+                  double deltaX = details.delta.dx;
+                  double deltaY = details.delta.dy;
+                  posX += deltaX;
+                  posY += deltaY;
+                });
+              },
+              child: Container(
+                width: boxSize,
+                height: boxSize,
+                decoration: BoxDecoration(color: Colors.red),
+              ),
+            ),
           ),
-        ),
+        ],
       ),
       bottomNavigationBar: Container(
         color: Colors.yellow,
@@ -66,5 +88,15 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+
+  void center(BuildContext context) {
+    posX = (MediaQuery.of(context).size.width / 2) - boxSize / 2;
+    posY = (MediaQuery.of(context).size.height / 2) - boxSize / 2 - 30;
+
+    setState(() {
+      this.posX = posX;
+      this.posY = posY;
+    });
   }
 }
